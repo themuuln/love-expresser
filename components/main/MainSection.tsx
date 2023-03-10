244d4363380178de5638994ee371b66098aacfe0
themuuln <zerone.offical@gmail.com>
2023-03-10 01:41:34 -0800

    cleanup: cleanup complete

--- components/main/MainSection.tsx
+++ components/main/MainSection.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import CreatorSection from "./CreatorSection";
-import VisitorSection from "./VisitorSection";
 
 const MainSection = () => {
   return (
     <>
-      <div className="flex">
-        <CreatorSection />
-        <VisitorSection />
-      </div>
+      <CreatorSection />
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,8 @@
-import styles from "@/styles/Home.module.css";
-import HeadSection from "@/components/HeadSection";
-import HeaderSection from "@/components/header/HeaderSection";
-import CreatorSection from "@/components/main/CreatorSection";
-import FooterSection from "@/components/footer/FooterSection";
-import VisitorSection from "@/components/main/VisitorSection";
 import MainSection from "@/components/main/MainSection";
 
 export default function Home() {
   return (
     <>
-      <HeadSection />
       <MainSection />
     </>
   );
